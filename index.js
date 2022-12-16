698781d29a3b114e396e92e2e5f78eb3c279f411
suraj <surajguptabnp14c@gmail.com>
2022-12-16 20:08:21 +0530

    connected to the database

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
+const connection = "./database.server.js"
+const cors = require("cors");
 
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Homepage");
+})
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.listen(PORT, () => {
+    connection && console.log("connected to database");
+    console.log(`server is running on port ${PORT}`)
+})
